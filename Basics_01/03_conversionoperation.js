364cead819f7e49c14c48a83809dfb1ca70aabaf
sanchitas-github <158267860+sanchitas-github@users.noreply.github.com>
2024-10-09 17:24:15 +0000

    operations

--- Basics_01/03_conversionoperation.js
+++ Basics_01/03_conversionoperation.js
@@ -88,3 +88,36 @@ string
 number
 NaN
 */
+
+//******************* OPERATIONS***************//
+//Arithmetic operations//
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);*/
+let value=3
+let negvalue= -value;
+
+let str1="hello";
+let str2="sanchita";
+let str3=str1+str2;
+console.log(str3)// output => hello sanchita
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+//If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+let num1,num2,num3
+num1=num2=num3=2+2
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);//101
+//If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+let num11,num12,num13
+num11=num12=num13=2+2
+let gameCounter1=100
+++gameCounter1;
+console.log(gameCounter1);//101
