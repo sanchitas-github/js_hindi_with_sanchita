6b912bb075596e10fd2c612f99bbdbdacb70ab21
sanchitas-github <158267860+sanchitas-github@users.noreply.github.com>
2024-10-09 16:02:06 +0000

    datatypes

---
+++ Basics_01/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js code as newer version
+//alert(3+3)// we are using node js not browser
+let name="sanchita"
+let age="19"
+let isLoggedIn=false
+let state;
+//number=>2 to power 53
+//bigint trading stock markets etc for big numbers
+//string =>""
+//boolean=> true false
+//null=>standalone value as well as datatype its an object.
+//undefined=>
+//symbol=>uniqueness find krne ke liye symbols ka use kiya jata hai
+
+
+//object=
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
